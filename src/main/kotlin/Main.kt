8ee6ba34921eebed110abc1f1971250c848e5fe1
Ryann <rmahajan4@wisc.edu>
2022-12-14 19:02:40 -0600

    Initial Commit. Didnt add challenges yet

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,30 @@
+var heroName: String = ""
+
+fun main() {
+    heroName =  promptHeroName()
+    narrate("$heroName, ${createTitle(heroName)}, heads to the town square")
+    visitTavern()
+}
+
+private fun promptHeroName(): String {
+    narrate("A hero enters the town of Kronstadt. What is their name?") { message ->
+    // Prints the message in yellow
+        "\u001b[33;1m$message\u001b[0m"
+    }
+    /*val input = readLine()
+    require(input != null && input.isNotEmpty()) {
+    "The hero must have a name."
+    }
+    return input*/
+    println("Madrigal")
+    return "Madrigal"
+}
+
+private fun createTitle(name: String): String {
+    return when {
+        name.all { it.isDigit() } -> "The Identifiable"
+        name.none { it.isLetter() } -> "The Witness Protection Member"
+        name.count { it.lowercase() in "aeiou" } > 4 -> "The Master of Vowels"
+        else -> "The Renowned Hero"
+    }
+}
---
+++ src/main/kotlin/Tavern.kt
@@ -0,0 +1,47 @@
+import java.io.File
+
+private const val TAVERN_MASTER = "Taernyl"
+private const val TAVERN_NAME = "$TAVERN_MASTER's Folly"
+
+private val firstNames = setOf("Alex", "Mordoc", "Sophie", "Tariq")
+private val lastNames = setOf("Ironfoot", "Fernsworth", "Baggins", "Downstrider")
+
+private val menuData = File("data/tavern-menu-data.txt")
+    .readText()
+    .split("\n")
+
+private val menuItems = List(menuData.size) { index ->
+    val (_, name, _) = menuData[index].split(",")
+    name
+}
+
+fun visitTavern() {
+    narrate("$heroName enters $TAVERN_NAME")
+
+    print_menu()
+
+    val patrons: MutableSet<String> = mutableSetOf()
+    repeat(10) {
+        patrons += "${firstNames.random()} ${lastNames.random()}"
+    }
+
+    narrate("$heroName sees several patrons in the tavern:")
+    narrate(patrons.joinToString())
+    repeat(3) {
+        placeOrder(patrons.random(), menuItems.random())
+    }
+
+}
+
+private fun print_menu() {
+    narrate("There are several items for sale:")
+    println(menuItems.joinToString())
+
+//    val longestMenuItemLength = menuItems.maxByOrNull { it.length }
+//    println(longestMenuItemLength)
+}
+
+private fun placeOrder(patronName: String, menuItemName: String) {
+    narrate("$patronName speaks with $TAVERN_MASTER to place an order")
+    narrate("$TAVERN_MASTER hands $patronName a $menuItemName")
+}
